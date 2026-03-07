e4950eded3089bcbab29810a309e9167be905840
Ava Dean <ava.dean@york.ac.uk>
2026-03-07 18:40:28 +0000

    Remove output files generated by the regression test

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -23,4 +23,10 @@ def test_AR2plus(snaptolshot):
 
     assert snaptolshot == output_lines
 
+    pathlib.Path("data/meshnodes.dat").unlink()
+    pathlib.Path("data/TECPlot2D/2DPlots_Data/AR2+.csv").unlink()
+    pathlib.Path("data/TECPlot2D/2DPlot_AR2+.png").unlink()
+    pathlib.Path("data/TECPlot2D/2DPlots_Data").rmdir()
+    pathlib.Path("data/TECPlot2D").rmdir()
+
     os.chdir("../")
